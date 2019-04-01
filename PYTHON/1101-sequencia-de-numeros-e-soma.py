6ee928981d824a4e2c22ccd1e1bc39d1835ecf99
Wellyson Martins <wellysonmartins@gmail.com>
2019-04-01 12:03:07 -0300

    1101 - PYTHON

---
+++ PYTHON/1101-sequencia-de-numeros-e-soma.py
@@ -0,0 +1,17 @@
+while True:
+   m, n = map(int, input().split(" "))
+   if m <= 0 or n <= 0:
+      break
+   soma = 0
+   numeros = ''
+   if m > n:
+      for i in range(n, m+1, 1):
+         soma+=i
+         numeros += str(i) + ' '
+   else:
+      for i in range(m, n+1, 1):
+         soma+=i
+         numeros += str(i) + ' '
+   
+   numeros += 'Sum={}'
+   print(numeros.format(soma))
